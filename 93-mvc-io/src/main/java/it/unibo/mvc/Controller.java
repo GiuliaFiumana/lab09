ba0ca5617fc65ac1dce8afcdcb814f8de53410ce
Giulia Fiumana <giulia.fiumana4@studio.unibo.it>
2025-11-18 14:48:41 +0100

    wrote the interface Controller

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,37 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    /**
+     * method for setting the next string to print.
+     * 
+     * @param stringa to print
+     */
+    void setNextStringToPrint(String stringa);
+
+    /**
+     * method to get the next string to print.
+     *
+     * @return
+     */
+    String getNextStringToPrint();
+
+    /**
+     * method to get the history og the printed strings.
+     * 
+     * @return
+     */
+    List<String> getHistoryOfPrintedStrings();
+    
+    /**
+     * method to print the current string.
+     * 
+     */
+    void printCurrentString();
 
 }
